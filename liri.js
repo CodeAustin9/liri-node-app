9a4c682af7fba5398150530067871e3201874845
Code Austin 9 <630smileaa@gmail.com>
2018-05-24 14:53:53 -0600

    added new screenshots

--- liri.js
+++ liri.js
@@ -7,16 +7,18 @@ var Twitter = require('twitter');
 var userinput = process.argv[2];
 var Spotify = require('node-spotify-api');
 var userInput2 = process.argv[3];     
+var fs = require("fs");
+
 
 function twitter() {
     var client = new Twitter(keys.twitter);
-    console.log("working");
     var params = { screen_name: 'realDonaldTrump' };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
 
             for (var i = 0; i < 20; i++) {
                 console.log(tweets[i].text);
+                console.log("------------");
             }
         }
     });
@@ -44,13 +46,47 @@ var query = userInput2;
 
 
 }
-function omdb() {
-
-request("http://www.omdbapi.com/?apikey=trilogy&t=Mr.+Nobody", function(error, response, body) {
-    console.log(JSON.parse(body));
+function omdb(userInput2) {
+    var query = userInput2;
+    if(!userInput2) {
+        query = 'Mr. Nobody';
+    }
+request("http://www.omdbapi.com/?apikey=trilogy&t=" + query, function(error, response, body) {
+    var jsonData = JSON.parse(body);
+ 
+    console.log("Movie Title: " + jsonData.Title);
+    console.log("Movie Year: " + jsonData.Year);
+    console.log("Movie Release: " + jsonData.Released);
+    console.log("Rotten Tomatoes Rating: " + jsonData.Ratings[1].Source);
+    console.log("Movie Country: " + jsonData.Country);
+    console.log("Movie Language: " + jsonData.Language);
+    console.log("Movie Plot: " + jsonData.Plot);
+    console.log("Movie Actors: " + jsonData.Actors);
+   
+ 
 });
 
 } function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        if (error) {
+            return console.log(error);
+        }
+        
+
+        // Then split it by commas (to make it more readable)
+        // .split converts string into array
+        // .join converts array into string
+        var actionsArr = data.split(", ");
+
+        // We will then re-display the content as an array for later use.
+       
+
+        
+        userInput2 = actionsArr[1];
+        userinput = actionsArr[0];
+        userInput();
+    });
 
 }
 
@@ -62,10 +98,11 @@ function userInput() {
         spotify(userInput2);
     }
     else if (userinput === "movie-this") {
-        omdb()
+        omdb(userInput2);
     }
     else if (userinput === "do-what-it-says") {
         doWhatItSays()
+    
     }
 
 }
